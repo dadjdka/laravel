f2051652fd240e5a9fc33015740fc1ea99b8c767
zseujmzx123456 <254707268@qq.com>
2020-02-23 19:19:19 +0800

    oss修正

--- blog/app/Http/Controllers/Component/OssController.php
+++ blog/app/Http/Controllers/Component/OssController.php
@@ -1,9 +1,21 @@
 <?php
 
 namespace App\Http\Controllers\Component;
-
+if (is_file(__DIR__ . '/../autoload.php')) {
+    require_once __DIR__ . '/../autoload.php';
+}
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use OSS\Core\OssException as CoreOssException;
+// use App\Services\OSS;
+// use Aliyun\OSS\OSSClient;
+use OSS\OssClient;
+
+use OSS\Core\OSSException;
+
 
 class OssController extends Controller
 {
@@ -13,53 +25,89 @@ class OssController extends Controller
        $vid = $request->file('vid');
         // dd($vid->getClientOriginalExtension());
         // dd($vid->getRealPath());
-       $id = 'LTAI4FgLj2Cfe75q61BEeAbW';
-       $key = 'bGpvpAcHuu0QzpG2k6xHm1CiRU7Swg';
-       $host = 'http://laravel6-video.oss-cn-beijing.aliyuncs.com';
+    //    $id = 'LTAI4FgLj2Cfe75q61BEeAbW';
+    //    $key = 'bGpvpAcHuu0QzpG2k6xHm1CiRU7Swg';
+    //    $host = 'http://laravel6-video.oss-cn-beijing.aliyuncs.com';
 
-       $dir = $_GET['dir'];
+       $dir = "video";
+
+    // dd($vid->getRealPath());
+
+        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        $accessKeyId = "LTAI4FgLj2Cfe75q61BEeAbW";
+        $accessKeySecret = "bGpvpAcHuu0QzpG2k6xHm1CiRU7Swg";
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        $endpoint = "http://oss-cn-beijing.aliyuncs.com";
+        // 存储空间名称
+        $bucket= "laravel6-video";
+        // 文件名称
+        $object = 'Cache__1dc5f74ff74f589..jpg';
+        // <yourLocalFile>由本地文件路径加文件名包括后缀组成，例如/users/local/myfile.txt
+        $filePath = "/Users/25470/Pictures/".$object;
+
+        // $config = array($accessKeyId,$accessKeySecret,$endpoint);
+
+        try{
+            $ossClient = new OssClient($accessKeyId,$accessKeySecret,$endpoint);
+            $ossClient->uploadFile($bucket, $object, $vid->getRealPath());
+        } catch(OssException $e) {
+            printf(__FUNCTION__ . ": FAILED\n");
+            printf($e->getMessage() . "\n");
+            return;
+        }
+        print(__FUNCTION__ . ": OK" . "\n");
 
 
-       function gmt_iso8601($time)
-       {
-            $dtStr = date("c",$time);
-            $mydatetime = new \DateTime($dtStr);
-            $expiration = $mydatetime->format(\DateTime::ISO8601);
-            $pos = strpos($expiration, '+');
-            $expiration = substr($expiration,0,$pos);
 
-            return $expiration."z";
-       }
 
-       $now = time();
-       $expire = 30; //policy 超时时间
-       $end = $now + $expire;
-       $expiration = gmt_iso8601($end);
 
-       //最大文件大小
-       $condition = [0 => 'content-length-range', 1 => '$key', 2 => $dir];
-       $condition[] = $condition;
 
-       //表示用户上传的数量，必须是以dir开始，不然上传会失败（不是必须项）
-    //    $start = [0 => 'starts-with', 1 => '$key', 2 => $dir];
-    //    $condition [] = $condition;
 
-       $arr = ['expiration' => $expiration, 'condition' => $condition];
-       $policy = json_encode($arr);
 
-       $base64_policy = \base64_encode($policy);
-       $string_to_sign = $base64_policy;
-       $signature = \base64_encode(hash_hmac('sha512',$string_to_sign,$key,true));
 
-       $response = [];
-       $response['accessid'] = $id;
-       $response['host'] = $host;
-       $response['policy'] = $base64_policy;
-       $response['signature'] = $signature;
-       $response['expire'] = $end;
-       $response['dir'] = $dir;
-    //    dd($response);
-       return json_encode($response);
+
+
+
+    //    function gmt_iso8601($time)
+    //    {
+    //         $dtStr = date("c",$time);
+    //         $mydatetime = new \DateTime($dtStr);
+    //         $expiration = $mydatetime->format(\DateTime::ISO8601);
+    //         $pos = strpos($expiration, '+');
+    //         $expiration = substr($expiration,0,$pos);
+
+    //         return $expiration."z";
+    //    }
+
+    //    $now = time();
+    //    $expire = 30; //policy 超时时间
+    //    $end = $now + $expire;
+    //    $expiration = gmt_iso8601($end);
+
+    //    //最大文件大小
+    //    $condition = [0 => 'content-length-range', 1 => '$key', 2 => $dir];
+    //    $condition[] = $condition;
+
+    //    //表示用户上传的数量，必须是以dir开始，不然上传会失败（不是必须项）
+    // //    $start = [0 => 'starts-with', 1 => '$key', 2 => $dir];
+    // //    $condition [] = $condition;
+
+    //    $arr = ['expiration' => $expiration, 'condition' => $condition];
+    //    $policy = json_encode($arr);
+
+    //    $base64_policy = \base64_encode($policy);
+    //    $string_to_sign = $base64_policy;
+    //    $signature = \base64_encode(hash_hmac('sha512',$string_to_sign,$key,true));
+
+    //    $response = [];
+    //    $response['accessid'] = $id;
+    //    $response['host'] = $host;
+    //    $response['policy'] = $base64_policy;
+    //    $response['signature'] = $signature;
+    //    $response['expire'] = $end;
+    //    $response['dir'] = $dir;
+    // //    dd($response);
+    //    return json_encode($response);
 
 
    }
--- blog/resources/views/admin/lesson/create.blade.php
+++ blog/resources/views/admin/lesson/create.blade.php
@@ -188,14 +188,86 @@ $(function () {
                 <textarea name="videos"  hidden>@{{videos}}</textarea>
             </div>
 
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <input type="text" class="form-control">
                 <span class="input-group-btn">
                     <button class="btn btn-default" type="button" id="pickVideo"> 上传文件</button>
                 </span>
 
-            </div>
+            </div> --}}
 
+            <div class="form-group">
+                <label for="" class="col-sm-2 control-label">上传文件</label>
+                <div class="col-sm-10">
+                    <input id="vid" type="file" name="vid"  class="form-control" data-min-file-count="1" required >
+                    {{-- <input type="text" class="form-control" name="preview"  required value="no.jpg"> --}}
+                </div>
+
+                <script>
+$(function () {
+//0.初始化fileinput
+var oFileInput = new FileInput();
+oFileInput.Init("vid", "/component/oss?");
+});
+var FileInput = function () {
+var oFile = new Object();
+
+//初始化fileinput控件（第一次初始化）
+oFile.Init = function (ctrlName, uploadUrl) {
+    var control = $('#' + ctrlName);
+
+    //初始化上传控件的样式
+    control.fileinput({
+        language: 'zh', //设置语言
+        uploadUrl: uploadUrl, //上传的地址
+        allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
+        //showUploadedThumbs:false,
+        // uploadClass: 'hidden',
+        showUpload: false, //是否显示上传按钮
+        showCaption: false,//是否显示标题
+        browseClass: "btn btn-info", //按钮样式
+        dropZoneEnabled: false,//是否显示拖拽区域
+        //minImageWidth: 150, //图片的最小宽度
+        //minImageHeight: 150,//图片的最小高度
+        //maxImageWidth: 150,//图片的最大宽度
+        //maxImageHeight: 150,//图片的最大高度
+        maxFileSize: 2048,//单位为kb，如果为0表示不限制文件大小
+        maxFileCount: 2, //表示允许同时上传的最大文件个数
+        minFileCount: 1,
+        enctype: 'multipart/form-data',
+        validateInitialCount: true,
+        previewFileIcon: "<i class='glyphicon glyphicon-king'></i>",
+        msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
+        fileActionSettings : {
+            // showUpload: false,
+            // showRemove: false,
+        //    showZoom:false
+        }
+    });
+
+
+
+    //导入文件上传完成之后的事件
+    $("#vid").on("fileuploaded", function (event, data, previewId, index) {
+        console.log('455')
+        alert(data.response.code);
+        // $("#divControl").hide();
+    });
+
+
+
+    $('#vid').on('fileerror', function(event, data, msg) {
+
+        console.log(data)
+
+    });
+}
+return oFile;
+};
+
+
+                </script>
+            </div>
 
             <button class="btn btn-primary">保存数据</button>
         </form>
